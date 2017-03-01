6473dbf693e51f570f6317233fa4662167d3cbc6
Tanner Fiscus <tanner@tannerfiscus.com>
2017-02-28 19:47:24 -0500

    Added eslint and config, app is not yet linted

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "array-bracket-spacing": [ "error", "always" ],
+        "arrow-parens": [ "error", "as-needed" ],
+        "camelcase": "error",
+        "indent": [ "error", 4 ],
+        "max-len": [ "off" ],
+        "padded-blocks": [ "off" ],
+        "semi": [ "error", "never" ]
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,13 @@ module.exports = {
   ],
 
   module: {
+    preLoaders: [
+        {
+            test: /\.(js|jsx)$/,
+            loader: 'eslint-loader',
+            exclude: /node_modules/,
+        },
+    ],
     loaders: [
       {
           test: /\.(js|jsx)$/,
