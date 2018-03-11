7e8e2917dcf4d5d718d39c39855a9f2c26f2b388
Tanner Fiscus <tanner@tannerfiscus.com>
2018-03-11 13:09:20 -0700

    Handle trailing slashes

--- server.js
+++ server.js
@@ -6,28 +6,37 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare()
-.then(() => {
-  const server = express()
+    .then(() => {
+        const server = express()
 
-  server.get('/portfolio/:id', (req, res) => {
-    // Should verify that the id exists and throw a 404 if not
-    const actualPage = '/portfolio-item'
-    const queryParams = { id: req.params.id }
-    app.render(req, res, actualPage, queryParams)
-  })
+        server.use((req, res, next) => {
+            const test = /\?[^]*\//.test(req.url);
+            if (req.url.substr(-1) === '/' && req.url.length > 1 && !test) {
+                res.redirect(301, req.url.slice(0, -1));
+            } else {
+                next()
+            }
+        })
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+        server.get('/portfolio/:id', (req, res) => {
+            // Should verify that the id exists and throw a 404 if not
+            const actualPage = '/portfolio-item'
+            const queryParams = { id: req.params.id }
+            app.render(req, res, actualPage, queryParams)
+        })
 
-  const port = process.env.PORT || 8080
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
 
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+        const port = process.env.PORT || 8080
+
+        server.listen(port, (err) => {
+            if (err) throw err
+            console.log(`> Ready on http://localhost:${port}`)
+        })
+    })
+    .catch((ex) => {
+        console.error(ex.stack)
+        process.exit(1)
+    })
