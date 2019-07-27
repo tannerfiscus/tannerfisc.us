08fea7868c63cf0d83e81d19b78d4e507ad39596
Tanner Fiscus <tanner@tannerfiscus.com>
2019-07-26 17:03:38 -0700

    Update apollo client url

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export default new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: process.env.NODE_ENV === 'production' ? "/graphql" : "http://localhost:4000/graphql",
 });
