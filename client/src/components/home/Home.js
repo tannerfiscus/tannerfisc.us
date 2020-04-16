1dfc4e564d99d47ff38a37de74e1c3cb7b41e328
Tanner Fiscus <tanner@tannerfiscus.com>
2020-04-15 22:12:33 -0400

    Updating home base

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -12,7 +12,7 @@ const Home = () => (
             meta={ [
                 { name: 'description', content: 'Hi, I\'m Tanner. I\'m a front-end engineer living in the bay area. I have a passion for building a better web.' },
                 { property: 'og:title', content: 'Tanner Fiscus' },
-                { property: 'og:description', content: 'Front-end developer and designer living in San Francisco, CA.' },
+                { property: 'og:description', content: 'Front-end developer and designer living in Toronto, Canada.' },
                 { property: 'og:type', content: 'website' },
                 { property: 'og:url', content: 'https://tannerfisc.us' },
                 { property: 'og:image', content: 'https://tannerfisc.us/images/og/home.jpg' }
@@ -26,7 +26,7 @@ const Home = () => (
                     <span aria-label="Hand wave" className={styles['wave-emoji']} role="img">👋</span>
                 </div>
                 <h1 className={styles['home-intro-title']}>Hi, I'm Tanner.</h1>
-                <Text className={styles['home-intro-text']}>I'm a San Francisco-based front-end engineer.</Text>
+                <Text className={styles['home-intro-text']}>I'm a Toronto-based front-end engineer.</Text>
             </div>
         </main>
     </Page>
